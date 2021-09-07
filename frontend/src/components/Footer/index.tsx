aa2f5e7a8d90ae6c37224064cd8ef08ea40e4b2d
Barbara Leles <barbara_rlf@hotmail.com>
2021-09-06 21:29:03 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/barbaraleles" target="_blank" rel="noreferrer">Barbara Leles</a></p>
+                </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
